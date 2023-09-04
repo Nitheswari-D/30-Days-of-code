a6d86816fb756cb045eca94f314f036bcc73c36b
Nitheswari.D <103818470+Nitheswari-D@users.noreply.github.com>
2023-09-04 23:51:54 +0530

    day_27

---
+++ week4/xor.java
@@ -0,0 +1,13 @@
+//xor operation in an array
+class Solution {
+    public int xorOperation(int n, int start) {
+        int arr[]=new int[n];
+        arr[0]=start+2*0;
+        int val=arr[0];
+        for(int i=1;i<n;i++){
+            arr[i]=start+2*i;
+            val^=arr[i];
+        }
+        return val;
+    }
+}
